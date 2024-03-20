fd8257081489eb12dd6165a4520a890e05ef78c6
VinaySingh7112 <vinaysinghraksel11@gmail.com>
2024-03-20 10:41:23 +0530

    1
    1

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const Navbar = () => {
+  return (
+    <div className='d-flex'>
+        <div>
+          <img width={"100px"} src="card2.png" alt="" />
+          <span> SHOPPER</span>
+        </div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+export default Navbar;
